16d25da6a5ad424427077867b491e08306652f3a
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 13:35:00 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -2,11 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Image Captioning Group 13')
 
 
 
-
-text1="hello world"
-
-print(text1)
