b6c056e4ae1098c6b2a2dd78d2dd643e8f6be2c7
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 16:53:11 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -11,12 +11,12 @@ st.text('textual description of images we will be using Deep Learning Techniques
 
 
 st.write("""
-#Image Captioning Group 13'
+# Image Captioning Group 13
 
 Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators 
 of the app ,,Image captioning“. Our app’s aim is to automatically describe an image with 
 one or more natural language sentences. To generate textual description of images we will 
-be using Neural Network and Deep Learning Techniques
+be using Neural Network and Deep Learning Techniques.
 
 
 """)
