b39f9d91272624fd12d63dd4b3af3bada9353409
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 13:51:14 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('Image Captioning Group 13')
-
+st.title('Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators of the app ')
+st.title('Image captioning“. Our app’s aim is to automatically describe an image with one or more natural language sentences. ')
+st.title('To generate textual description of images we will be using Neural Network and Deep Learning Techniques.')
 
 
