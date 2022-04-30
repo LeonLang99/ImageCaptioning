a37c876f4670ef8d341a526fed705aaec7ca0ffe
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 16:51:58 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -10,6 +10,17 @@ st.text('describe an image with one or more natural language sentences. To gener
 st.text('textual description of images we will be using Deep Learning Techniques.')
 
 
+st.write("""
+#Image Captioning Group 13'
+
+Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators 
+of the app ,,Image captioning“. Our app’s aim is to automatically describe an image with 
+one or more natural language sentences. To generate textual description of images we will 
+be using Neural Network and Deep Learning Techniques
+
+
+""")
+
 from datasets import load_dataset
 
 dataset = load_dataset("laion/laion2B-multi")
