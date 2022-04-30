58545f580c7571e380075d56e7a9a39357539746
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 16:53:55 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Image Captioning Group 13')
-
-st.text('Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the')
-st.text('creators of the app Image captioning“. Our app’s aim is to automatically')
-st.text('describe an image with one or more natural language sentences. To generate')
-st.text('textual description of images we will be using Deep Learning Techniques.')
-
 
 st.write("""
 # Image Captioning Group 13
@@ -20,9 +13,5 @@ be using Neural Network and Deep Learning Techniques.
 
 
 """)
-
-from datasets import load_dataset
-
-dataset = load_dataset("laion/laion2B-multi")
 
 
