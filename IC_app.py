8e24dde082963621e4d876027fa543624e263d44
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-04-30 13:54:08 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -7,6 +7,6 @@ st.title('Image Captioning Group 13')
 st.text('Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the')
 st.text('creators of the app Image captioning“. Our app’s aim is to automatically')
 st.text('describe an image with one or more natural language sentences. To generate')
-st.text(' textual description of images we will be using Neural Network and Deep Learning Techniques.')
+st.text('textual description of images we will be using Deep Learning Techniques.')
 
 
