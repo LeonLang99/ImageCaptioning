57373347cadce56237a5615de005d31a1c4d8ddb
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 15:13:21 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -14,7 +14,7 @@ st.write("""
 Hello, we are Leon Lang, Jean Luis Fichtner and Loredana Bratu and we are the creators 
 of the app ,,Image captioning“. Our app’s aim is to automatically describe an image with 
 one or more natural language sentences. To generate textual description of images we will 
-be using Neural Network and Deep Learning Techniques.
+be using Neural Network and Deep Learning Techniques. hello im Lou
 
 
 """)
