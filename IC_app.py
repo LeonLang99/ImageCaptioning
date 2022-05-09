71405a67cf78cd59ed5a0fafec9ba804bd95bcd8
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-05-09 21:10:32 +0200

    Update IC_app.py

--- IC_app.py
+++ IC_app.py
@@ -35,9 +35,9 @@ with st.expander("Random Picture"):
   
   
 with st.expander("Our vision..."):    
-   st.text("# What are our project objectives?")
+   st.write("# What are our project objectives?")
    st.write("Our main goal is our app to automatically generate captions, also known as textual descriptions, for random images. The dataset will be in the form [image → captions]. It will consist of input images and their corresponding output captions which have to be as precise as possible, but also short and concise. The caption generator will involve the dual techniques from computer vision - to first understand the content of the image, and a language model from the field of natural language processing to turn the understanding of the image into words in the right order and correct structure.")
-   st.text("#Which problem do we want to solve?")
+   st.write("#Which problem do we want to solve?")
    st.write("Image captioning can be used in lots of virtual domains such as for virtual assistants, picture recommendations, for image indexing, for social media, but also to explore the world around you, using only your phone's camera which scans a real object and tell someone what kind of object that is (for example Google Lens)."
-   st.text("#About our data…")
+   st.write("#About our data…")
    st.write("Our dataset consists of X images which are randomly sorted. For the training we will not use the whole data set, because it has an enormous storage capacity (~ 240TB of data).Therefore, in advantage of time and costs we will use between 500-1000 pictures to train our data."
